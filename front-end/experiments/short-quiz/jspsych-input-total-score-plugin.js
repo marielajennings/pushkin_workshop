827dcf2c48fee84c8a0bd692a90f622b91a4f756
Mariela Jennings <marielajennings@gmail.com>
2017-12-06 14:00:38 -0500

    better css

--- front-end/experiments/short-quiz/jspsych-input-total-score-plugin.js
+++ front-end/experiments/short-quiz/jspsych-input-total-score-plugin.js
@@ -31,10 +31,12 @@ plugin.trial= function (display_element, trial) {
 var calculated_percent=(trial.participant_score/trial.possible_score)*100
 
 var p = document.createElement('p');
+p.setAttribute('style', 'text-align:center; margin-top:100px')
+
 display_element.appendChild(p);
 p.innerHTML="Your score: "+trial.participant_score+"/"+trial.possible_score+""
 var div= document.createElement('div');
-div.setAttribute('style','background-color:#ddd; width: 100%; height:40px; border-radius: 25px;');
+div.setAttribute('style','background-color:#ddd; width: 800px; height:40px; border-radius: 25px; margin-top:50px; margin:0 auto');
 display_element.appendChild(div);
 var div2 = document.createElement('div');
 div2.setAttribute ("style","width:"+calculated_percent+"%; background-color:#00bcd4; border:00a5bb; text-align: right; padding-right: 20px; line-height: 40px; color: white; height:40px; border-radius: 25px;");
@@ -42,10 +44,11 @@ div.appendChild(div2);
 
 
 var div3=document.createElement('div');
-div3.setAttribute('style','margin-bottom:20px;');
+div3.setAttribute('style','margin-bottom:20px; margin-top:200px');
 display_element.appendChild(div3);
 var button = document.createElement('button');
 button.setAttribute('type','button');
+button.setAttribute('class','jspsych-btn');
 display_element.appendChild(button);
 button.innerHTML="Finish";
 button.addEventListener('click', () => {jsPsych.finishTrial({})});
